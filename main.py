e198c6573f09ee02bcde905d54870619a2ca1e03
MaggieLehman <mal4997@rit.edu>
2023-02-01 11:11:03 -0500

    First commit. Added main data file in individual project and use gitignore to ignore it. File name should be twcs.csv

---
+++ main.py
@@ -0,0 +1,11 @@
+#Data Mining Semester Project
+#Group Members:
+#Maggie Lehman
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+from scipy.stats import ttest_ind, ttest_rel
+from scipy import stats
